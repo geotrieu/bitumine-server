9fd445cf014542559d9e50530f3fab3b2f0815af
BluBloos <cnoah1705@gmail.com>
2021-12-18 23:31:07 -0500

    Mac MVP Complete

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 mkdir bin
-g++ -fdiagnostics-color=always -g src/address.cpp src/server.cpp src/socket.cpp -I src/headers -std=c++11 -o bin/server.exe -lwsock32
+g++ -fdiagnostics-color=always -g src/address.cpp src/server.cpp src/socket.cpp -I src/headers -std=c++11 -o bin/server.out
--- src/server.cpp
+++ src/server.cpp
@@ -50,4 +50,6 @@ int main() {
             }
         }
     }
+
+    socket.terminate();
 }
