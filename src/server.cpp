3da40ae801a8df6a63ab6cf7ce0b6d35209654bc
geotrieu <trieugeorge@gmail.com>
2021-12-19 12:06:40 -0500

    added ip address specification

--- src/server.cpp
+++ src/server.cpp
@@ -7,10 +7,14 @@ unsigned short dest_port;
 
 int main() {
 
+    std::string ip_address = "";
+
     std::cout << "Port for Socket" << std::endl;
     std::cin >> port; // Get user input from the keyboard
     std::cout << "Port for Destination Socket" << std::endl;
     std::cin >> dest_port; // Get user input from the keyboard
+    std::cout << "IP Address" << std::endl;
+    std::cin >> ip_address; // Get user input from the keyboard
     std::cout << "1 for Send, 2 for Receive" << std::endl;
     int mode;
     std::cin >> mode;
@@ -24,10 +28,24 @@ int main() {
 
     if (mode == 1) {
         //send packets
-        Address dest_address(127, 0, 0, 1, dest_port);
+        int ip_address_classes[] = {0, 0, 0, 0};
+
+        char *ip_address_part;
+        /* get the first token */
+        char * ip_address_cstr = new char [ip_address.length()+1];
+        strcpy(ip_address_cstr, ip_address.c_str());
+        ip_address_part = strtok(ip_address_cstr, ".");
+        
+        /* walk through other tokens */
+        for (int i = 0; i < 4; i++) {
+            ip_address_classes[i] = std::stoi(ip_address_part);
+            
+            ip_address_part = strtok(NULL, ".");
+        }
+        Address dest_address(ip_address_classes[0], ip_address_classes[1], ip_address_classes[2], ip_address_classes[3], dest_port);
 
         std::string phrase;
-        std::cout << "Enter phrase to send" << std::endl;
+        std::cout << "Enter phrase to send to: " << ip_address_classes[0] << "." << ip_address_classes[1] << "." << ip_address_classes[2] << "." << ip_address_classes[3] << std::endl;
         std::cin >> phrase;
         const char* packet_data = phrase.c_str();
         int packet_size = strlen(phrase.c_str());
